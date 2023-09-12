112253789e7af1349fcb9f7bd9242e5a59d41215
Nicholas <njhudz7@gmail.com>
2023-09-12 16:19:42 -0700

    retry

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,8 +1,10 @@
 const { PORT = 10000 } = process.env;
-const express = require("express")
-const app = require("./app")
+const express = require("express");
+const app = express();
 const knex = require("./db/connection");
+const path = require("path"); // Import the path module
 
+// Define your database migration and server startup logic
 knex.migrate
   .latest()
   .then((migrations) => {
@@ -14,7 +16,15 @@ knex.migrate
     knex.destroy();
   });
 
+// Catch-all route to serve your main HTML file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Configure static asset serving for your frontend assets (CSS, JavaScript, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
-} 
+}
 
