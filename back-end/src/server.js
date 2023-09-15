a3204107ca53c8a1dbd44a792fa449c614be80f1
Nicholas <njhudz7@gmail.com>
2023-09-15 00:51:45 -0600

    server.js changes/rollback

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,6 @@ const { PORT = 10000 } = process.env;
 const express = require("express")
 const app = require("./app")
 const knex = require("./db/connection");
-const history = require("connect-history-api-fallback");
-
-app.use(history())
 
 knex.migrate
   .latest()
